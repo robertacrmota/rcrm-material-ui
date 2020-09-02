098adab556bc7bd33aa25449b9cf702fce16259c
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-09-02 05:44:52 -0400

    add responsive social icons on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,13 @@ import {makeStyles} from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Hidden from '@material-ui/core/Hidden';
 
 import footerAdornment from '../assets/Footer Adornment.svg';
+import facebook from '../assets/facebook.svg';
+import twitter from '../assets/twitter.svg';
+import instagram from '../assets/instagram.svg';
+
 
 const useStyles = makeStyles(theme => ({
   footer: {
@@ -34,6 +39,19 @@ const useStyles = makeStyles(theme => ({
     fontSize: '0.75rem',
     fontWeight: 'bold',
     textDecoration: 'none'
+  },
+  socialContainer: {
+    position: 'absolute',
+    bottom: '.1rem',
+    right: '1rem'
+  },
+  socialIcon: {
+    width: '3rem',
+    height: '3rem',
+    [theme.breakpoints.down("xs")]: {
+      width: '1.5rem',
+      height: '1.5rem',
+    }
   }
 }));
 
@@ -42,42 +60,55 @@ export default function Footer(props) {
 
     return (
         <footer className={classes.footer}>
-        <Grid container justify='center' className={classes.gridContainer}>
-            <Grid item className={classes.gridItem}>
-              <Grid container direction='column'>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(0)} component={Link} to="/">Home</Grid>
+        <Hidden smDown>
+          <Grid container justify='center' className={classes.gridContainer}>
+              <Grid item className={classes.gridItem}>
+                <Grid container direction='column'>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(0)} component={Link} to="/">Home</Grid>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item className={classes.gridItem}>
-              <Grid container direction='column' spacing={2}>
-                  <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(0);}} component={Link} to="/services">Services</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(1);}} component={Link} to="/customsoftware">Custom Software Development</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(2);}} component={Link} to="/mobileapps">Mobile App Development</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(3);}} component={Link} to="/websites">Website Development</Grid>
+              <Grid item className={classes.gridItem}>
+                <Grid container direction='column' spacing={2}>
+                    <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(0);}} component={Link} to="/services">Services</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(1);}} component={Link} to="/customsoftware">Custom Software Development</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(2);}} component={Link} to="/mobileapps">Mobile App Development</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => {props.setTabValue(1); props.setSelectedMenuIndex(3);}} component={Link} to="/websites">Website Development</Grid>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item className={classes.gridItem}>
-              <Grid container direction='column' spacing={2}>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">The Revolution</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">Vision</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">Technology</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">Process</Grid>
+              <Grid item className={classes.gridItem}>
+                <Grid container direction='column' spacing={2}>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">The Revolution</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">Vision</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">Technology</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(2)} component={Link} to="/revolution">Process</Grid>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item className={classes.gridItem}>
-              <Grid container direction='column' spacing={2}>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(3)} component={Link} to="/about">About Us</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(3)} component={Link} to="/about">History</Grid>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(3)} component={Link} to="/about">Team</Grid>
+              <Grid item className={classes.gridItem}>
+                <Grid container direction='column' spacing={2}>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(3)} component={Link} to="/about">About Us</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(3)} component={Link} to="/about">History</Grid>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(3)} component={Link} to="/about">Team</Grid>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item className={classes.gridItem}>
-              <Grid container direction='column'>
-                  <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(4)} component={Link} to="/contact">Contact Us</Grid>
+              <Grid item className={classes.gridItem}>
+                <Grid container direction='column'>
+                    <Grid item className={classes.gridItemLink} onClick={() => props.setTabValue(4)} component={Link} to="/contact">Contact Us</Grid>
+                </Grid>
               </Grid>
-            </Grid>
+          </Grid>
+        </Hidden>
+        <img src={footerAdornment} className={classes.adornment} />
+        <Grid container justify={'flex-end'} spacing={2} className={classes.socialContainer}>
+          <Grid item component={'a'} href="#">
+            <img src={facebook} className={classes.socialIcon} />
+          </Grid>
+          <Grid item component={'a'} href="#">
+            <img src={twitter} className={classes.socialIcon}/>
+          </Grid>
+          <Grid item component={'a'} href="#">
+            <img src={instagram} className={classes.socialIcon}/>
+          </Grid>
         </Grid>
-        <img src={footerAdornment} className={classes.adornment}></img>
         </footer>
       );
 }
